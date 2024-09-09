31a9f390ff64004642f8f7909f04faac00f817a8
Diego Alcântara <ddag0309@gmail.com>
2024-09-09 01:38:02 -0300

    host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // Configuração do Vite
 export default defineConfig({
   plugins: [react()],
+  base: '/desafioTarefasFront/',
   server: {
     port: 5173, // Porta onde o Vite irá rodar
     historyApiFallback: true, // Configuração para tratar as rotas no React Router
